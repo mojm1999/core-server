e0bf6562c2cbc5c25e85f4623fdcb023089fd8d6
mojm <mojm1999@outlook.com>
2023-03-20 21:00:14 +0800

    读config

--- template/load_config.lua
+++ template/load_config.lua
@@ -7,7 +7,22 @@ local sep = package.config:sub(1,1)
 local current_path = [[.]]..sep
 local function include(filename)
     local last_path = current_path
-    local path, name = filename:match([[(.*)]]..sep..[[)(.*)$]])
+    local path, name = filename:match([[(.*]]..sep..[[)(.*)$]])
+    if path then
+        if path:sub(1,1) == sep then
+            current_path = path
+        else
+            current_path = current_path .. path
+        end
+    else
+        name = filename
+    end
+    local f = assert(io.open(current_path .. name))
+    local code = assert(f:read [[*a]])
+    code = string.gsub(code, [[%$([%w_%d]+)]], getenv)
+    f:close()
+    assert(load(code,[[@]]..filename,[[t]],result))
+    current_path = last_path
 end
 setmetatable(result, { __index = { include = include } })
 local config_name = ...
@@ -15,6 +30,8 @@ local config_name = ...
 setmetatable(result, nil)
 --return result
 
+--test
 local str123 = "./examples/config"
-local path, name = str123:match([[(.*)]]..sep..[[(.*)$]])
+--local path, name = str123:match([[(.*)]]..sep..[[(.*)$]])
+local path, name = str123:match([[(.*]]..sep..[[)(.*)$]])
 print(path, name)
